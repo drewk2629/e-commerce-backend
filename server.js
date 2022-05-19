72d1f43b776e2cf6bcc3a5de8e36dbdf61e5b5b0
Drew Knoeller <drewk2629@gmail.com>
2022-05-18 20:16:21 -0400

    models finished

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
